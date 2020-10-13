fb4f8f6f9415893139a532e786f93f47ad72c75d
unknown <shibut_d@mail.ru>
2020-10-13 08:49:30 -0700

    added python-dotenv

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path)
 
 
 class Config:
